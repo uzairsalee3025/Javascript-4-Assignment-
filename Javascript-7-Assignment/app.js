fe256cd849d6f07af082912652235c01fd1d4de4
Uzair Saleem <143824150+uzairsalee3025@users.noreply.github.com>
2023-10-10 05:47:32 +0500

    Add files via upload

---
+++ Javascript-7-Assignment/app.js
@@ -0,0 +1,16 @@
+// Events
+// Answer No 1
+var button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+    alert("Button Clicked!");
+});
+// Answer No 2
+const button = document.getElementById("clickMeButton");
+const elementToHide = document.getElementById("elementToHide");
+
+button.addEventListener("click", function() {
+    elementToHide.style.display = "none";
+});
+// Answer No 3
+
